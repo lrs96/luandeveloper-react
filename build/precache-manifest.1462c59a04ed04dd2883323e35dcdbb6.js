8736e36d406f318164ee491109702bf106cf85c0
Luan Rodrigues <internet29@kalunga.com.br>
2020-06-29 10:36:38 -0300

    build

---
+++ build/precache-manifest.1462c59a04ed04dd2883323e35dcdbb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef6343821c6875e9ddef56d4aa5ba3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fe85eeea630ef90e697",
+    "url": "/static/css/main.2a57a715.chunk.css"
+  },
+  {
+    "revision": "8cf7dcdc6509cc1d20e5",
+    "url": "/static/js/2.76cf5fe0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76cf5fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe85eeea630ef90e697",
+    "url": "/static/js/main.0274f19b.chunk.js"
+  },
+  {
+    "revision": "18ec23835bc88d959186",
+    "url": "/static/js/runtime-main.d8ce4a31.js"
+  },
+  {
+    "revision": "8de6fca21a6b65f42ae430cd13e51422",
+    "url": "/static/media/about-me-luan-rodrigues.8de6fca2.svg"
+  },
+  {
+    "revision": "0aff19be98c67e39f9810096b0d07666",
+    "url": "/static/media/luan-rodrigues-da-silva.0aff19be.svg"
+  }
+]);
